e55ee685ba699ac49742d8d05731a75c309d5f77
Rinzler8806 <shane.rogers916@gmail.com>
2021-05-25 12:27:21 -0700

    polished formatting on server.js file

--- Master/server.js
+++ Master/server.js
@@ -25,11 +25,11 @@ connection.connect(function (err) {
 
 function start() {
     inquirer.prompt({
-        name: 'action',
-        type: 'list',
-        message: 'What can we help you with today?',
-        choices: ['View department', 'view roles', 'view employees', 'add department', 'add employee', 'add role', 'update employee role', 'delete employee', 'delete department', 'delete role']
-    })
+            name: 'action',
+            type: 'list',
+            message: 'What can we help you with today?',
+            choices: ['View department', 'view roles', 'view employees', 'add department', 'add employee', 'add role', 'update employee role', 'delete employee', 'delete department', 'delete role']
+        })
         .then((res) => {
             if (res.action == 'View department') {
                 viewDept()
@@ -45,17 +45,14 @@ function start() {
                 addRole()
             } else if (res.action == 'update employee role') {
                 updateRole()
-            }
-            else if (res.action == 'delete employee') {
-                    deleteEmployee()
-            }
-            else if (res.action == 'delete department') {
+            } else if (res.action == 'delete employee') {
+                deleteEmployee()
+            } else if (res.action == 'delete department') {
                 deleteDept()
-            }
-            else if (res.action == 'delete role') {
+            } else if (res.action == 'delete role') {
                 deleteRole()
             }
-            })
+        })
 }
 
 function viewDept() {
@@ -94,8 +91,8 @@ function addDept() {
         console.log("Inserting a department...\n");
         connection.query(
             "INSERT INTO department SET ?", {
-            name: res.whichDept
-        },
+                name: res.whichDept
+            },
             function (err, res) {
                 if (err) throw err;
                 console.log(res.affectedRows + " department inserted!\n");
@@ -107,101 +104,100 @@ function addDept() {
 
 function addEmployee() {
     inquirer.prompt([{
-        name: 'firstName',
-        type: 'input',
-        message: 'What is the first name of the employee you would like to add?',
-    },
-    {
-        name: 'lastName',
-        type: 'input',
-        message: 'What is the last name of the employee you would like to add?',
-    },
-    {
-        name: 'roleID',
-        type: 'input',
-        message: 'What is the role ID of the employee?',
-    },
-    {
-        name: 'managerID',
-        type: 'input',
-        message: 'What is the ID of the employees manager?',
-    }])
-    .then((res) => {
-        console.log("Adding employee...\n");
-        connection.query(
-            "INSERT INTO employee SET ?", [
-        {
-            first_name: res.firstName,
-            last_name: res.lastName,
-            role_id: res.roleID,
-            manager_id: res.managerID
-        }
-        ],
-            function (err, res) {
-                if (err) throw err;
-                console.log(res.affectedRows + " employee added!\n");
-                start();
+                name: 'firstName',
+                type: 'input',
+                message: 'What is the first name of the employee you would like to add?',
+            },
+            {
+                name: 'lastName',
+                type: 'input',
+                message: 'What is the last name of the employee you would like to add?',
+            },
+            {
+                name: 'roleID',
+                type: 'input',
+                message: 'What is the role ID of the employee?',
+            },
+            {
+                name: 'managerID',
+                type: 'input',
+                message: 'What is the ID of the employees manager?',
             }
-        );
-    })
+        ])
+        .then((res) => {
+            console.log("Adding employee...\n");
+            connection.query(
+                "INSERT INTO employee SET ?", [{
+                    first_name: res.firstName,
+                    last_name: res.lastName,
+                    role_id: res.roleID,
+                    manager_id: res.managerID
+                }],
+                function (err, res) {
+                    if (err) throw err;
+                    console.log(res.affectedRows + " employee added!\n");
+                    start();
+                }
+            );
+        })
 }
 
 function addRole() {
     inquirer.prompt([{
-        name: 'title',
-        type: 'input',
-        message: 'What is the name of the role you would like to add?',
-    },
-    {
-        name: 'salary',
-        type: 'input',
-        message: 'What is the salary for the role being added?',
-    },
-    {
-        name: 'departmentID',
-        type: 'input',
-        message: 'What department ID number is the new role listed under?',
-    }])
-    .then((res) => {
-        console.log("Adding role...\n");
-        connection.query(
-            "INSERT INTO role SET ?", [
-        {
-            title: res.title,
-            salary: res.salary,
-            department_id: res.departmentID
-        }
-        ],
-            function (err, res) {
-                if (err) throw err;
-                console.log(res.affectedRows + " role added!\n");
-                start();
+                name: 'title',
+                type: 'input',
+                message: 'What is the name of the role you would like to add?',
+            },
+            {
+                name: 'salary',
+                type: 'input',
+                message: 'What is the salary for the role being added?',
+            },
+            {
+                name: 'departmentID',
+                type: 'input',
+                message: 'What department ID number is the new role listed under?',
             }
-        );
-    })
+        ])
+        .then((res) => {
+            console.log("Adding role...\n");
+            connection.query(
+                "INSERT INTO role SET ?", [{
+                    title: res.title,
+                    salary: res.salary,
+                    department_id: res.departmentID
+                }],
+                function (err, res) {
+                    if (err) throw err;
+                    console.log(res.affectedRows + " role added!\n");
+                    start();
+                }
+            );
+        })
 }
 
 function updateRole() {
     inquirer.prompt([{
-        name: 'employeeID',
-        type: 'input',
-        message: 'What is the employees id that you would like to update?',
-    },
-    {
-        name: 'role_id',
-        type: 'input',
-        message: 'What is the employees new role?',
-    }])
+                name: 'employeeID',
+                type: 'input',
+                message: 'What is the employees id that you would like to update?',
+            },
+            {
+                name: 'role_id',
+                type: 'input',
+                message: 'What is the employees new role?',
+            }
+        ])
         .then((res) => {
             console.log("Updating employee role...\n");
             var query = connection.query(
                 "UPDATE employee SET ? WHERE ?",
                 [{
-                    role_id: res.role_id
-                },
-                {
-                    id: res.employeeID
-                },
+                        role_id: res.role_id
+                    },
+                    {
+                        id: res.employeeID
+                    },
                 ],
                 function (err, res) {
                     if (err) throw err;
@@ -215,14 +211,13 @@ function updateRole() {
 function deleteEmployee() {
     console.log("Deleting employee...\n");
     inquirer.prompt({
-        name: 'employeeID',
-        type: 'input',
-        message: 'What is the employees id that you would like to delete?',
-    })
+            name: 'employeeID',
+            type: 'input',
+            message: 'What is the employees id that you would like to delete?',
+        })
         .then((res) => {
             connection.query(
-                "DELETE FROM employee WHERE ?",
-                {
+                "DELETE FROM employee WHERE ?", {
                     id: res.employeeID
                 },
                 function (err, res) {
@@ -237,14 +232,13 @@ function deleteEmployee() {
 function deleteDept() {
     console.log("Deleting department...\n");
     inquirer.prompt({
-        name: 'departmentID',
-        type: 'input',
-        message: 'What is the departments id that you would like to delete?',
-    })
+            name: 'departmentID',
+            type: 'input',
+            message: 'What is the departments id that you would like to delete?',
+        })
         .then((res) => {
             connection.query(
-                "DELETE FROM department WHERE ?",
-                {
+                "DELETE FROM department WHERE ?", {
                     id: res.departmentID
                 },
                 function (err, res) {
@@ -259,14 +253,13 @@ function deleteDept() {
 function deleteRole() {
     console.log("Deleting role...\n");
     inquirer.prompt({
-        name: 'roleID',
-        type: 'input',
-        message: 'What is the id of the role that you would like to delete?',
-    })
+            name: 'roleID',
+            type: 'input',
+            message: 'What is the id of the role that you would like to delete?',
+        })
         .then((res) => {
             connection.query(
-                "DELETE FROM role WHERE ?",
-                {
+                "DELETE FROM role WHERE ?", {
                     id: res.roleID
                 },
                 function (err, res) {
@@ -276,4 +269,4 @@ function deleteRole() {
                 }
             )
         });
-    }
+}
